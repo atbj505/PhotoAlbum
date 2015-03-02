9eff62aceb4da068bddec8b502981227eb8e126c
atbj505 <atbj505@hotmail.com>
2015-03-02 16:46:24 +0800

    nsobject test

---
+++ PhotoAlbum/PhotoAlbum/photoLibrary.h
@@ -0,0 +1,13 @@
+//
+//  photoLibrary.h
+//  PhotoAlbum
+//
+//  Created by Robert on 15/3/2.
+//  Copyright (c) 2015年 NationSky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface photoLibrary : NSObject
+
+@end
---
+++ PhotoAlbum/PhotoAlbum/photoLibrary.m
@@ -0,0 +1,13 @@
+//
+//  photoLibrary.m
+//  PhotoAlbum
+//
+//  Created by Robert on 15/3/2.
+//  Copyright (c) 2015年 NationSky. All rights reserved.
+//
+
+#import "photoLibrary.h"
+
+@implementation photoLibrary
+
+@end
